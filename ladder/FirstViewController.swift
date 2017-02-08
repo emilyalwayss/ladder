c0fddb806be93044f0c09f380ab1c9e7608505df
Emily Chen <Emily@emilys-macbook-air.local>
2017-02-07 22:05:00 -0500

    Initial Commit

---
+++ ladder/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ladder
+//
+//  Created by Emily Chen on 2/7/17.
+//  Copyright © 2017 Emily Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
